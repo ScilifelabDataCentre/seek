464ef9b9805892399057dd57e4a30a1a48cf7908
Sonja Mathias <sonja.mathias@scilifelab.uu.se>
2024-01-22 12:58:52 +0100

    Fix jws tests

--- test/functional/jws_online_test.rb
+++ test/functional/jws_online_test.rb
@@ -8,39 +8,45 @@ class JwsOnlineTest < ActionController::TestCase
   include JwsOnlineTestHelper
 
   test 'simulate button visibility' do
-    model = FactoryBot.create(:teusink_model, policy: FactoryBot.create(:public_policy))
-    get :show, params: { id: model }
-    assert_response :success
-    assert_select '#buttons a[href=?]', simulate_model_path(model, version: 1)
+    with_config_value(:jws_enabled, true) do
+      model = FactoryBot.create(:teusink_model, policy: FactoryBot.create(:public_policy))
+      get :show, params: { id: model }
+      assert_response :success
+      assert_select '#buttons a[href=?]', simulate_model_path(model, version: 1)
 
-    model = FactoryBot.create(:non_sbml_xml_model, policy: FactoryBot.create(:public_policy))
-    get :show, params: { id: model }
-    assert_response :success
-    assert_select '#buttons a[href=?]', simulate_model_path(model, version: 1), count: 0
+      model = FactoryBot.create(:non_sbml_xml_model, policy: FactoryBot.create(:public_policy))
+      get :show, params: { id: model }
+      assert_response :success
+      assert_select '#buttons a[href=?]', simulate_model_path(model, version: 1), count: 0
 
-    model = FactoryBot.create(:teusink_model, policy: FactoryBot.create(:publicly_viewable_policy))
-    get :show, params: { id: model }
-    assert_response :success
-    assert_select '#buttons a[href=?]', simulate_model_path(model, version: 1), count: 0
+      model = FactoryBot.create(:teusink_model, policy: FactoryBot.create(:publicly_viewable_policy))
+      get :show, params: { id: model }
+      assert_response :success
+      assert_select '#buttons a[href=?]', simulate_model_path(model, version: 1), count: 0
+    end
   end
 
   test 'simulate' do
-    model = FactoryBot.create(:teusink_model, policy: FactoryBot.create(:public_policy))
-    get :simulate, params: { id: model.id, version: model.version, constraint_based:'1' }
-    assert_response :success
-    assert assigns(:simulate_url)
+    with_config_value(:jws_enabled, true) do
+      model = FactoryBot.create(:teusink_model, policy: FactoryBot.create(:public_policy))
+      get :simulate, params: { id: model.id, version: model.version, constraint_based:'1' }
+      assert_response :success
+      assert assigns(:simulate_url)
 
-    url = assigns(:simulate_url)
-    refute_nil url
-    assert url =~ URI.regexp, "simulate url (#{url}) should be a valid url"
-    assert_select 'iframe[src=?]', url
+      url = assigns(:simulate_url)
+      refute_nil url
+      assert url =~ URI.regexp, "simulate url (#{url}) should be a valid url"
+      assert_select 'iframe[src=?]', url
+    end
   end
 
   test 'simulate no constraint defined' do
-    model = FactoryBot.create(:teusink_model, policy: FactoryBot.create(:public_policy))
-    get :simulate, params: { id: model.id, version: model.version }
-    assert_response :success
-    refute assigns(:simulate_url)
-    assert_select 'input[@type=checkbox]#constraint_based',count: 1
+    with_config_value(:jws_enabled, true) do
+      model = FactoryBot.create(:teusink_model, policy: FactoryBot.create(:public_policy))
+      get :simulate, params: { id: model.id, version: model.version }
+      assert_response :success
+      refute assigns(:simulate_url)
+      assert_select 'input[@type=checkbox]#constraint_based',count: 1
+    end
   end
 end
